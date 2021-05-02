0cd93c764d27cb17a1a5a4fadd95ce5058a17827
rami <ramis@avetti.ca>
2021-05-02 05:44:43 +0300

    fix issue 2 3

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 const publicPath = path.join(__dirname, '..', 'build');
 app.use(express.static(publicPath));
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     theme: {
       colors: {
-        mainBgColor: "#212121",
-        mainTextColor: "#212121",
-        mainlightTextColor: "#F8F8F8",
+        mainBgColor: '#212121',
+        mainTextColor: '#212121',
+        mainlightTextColor: '#F8F8F8',
       },
     },
   },
